e8213c5ed382fbee525241a79d83e56ae96b42f9
briskt <3172830+briskt@users.noreply.github.com>
2026-01-15 18:28:03 +0800

    copy code from github.com/sil-org/terraform-modules
    Using terraform-modules version 8.14.3 as starting point for this module. The aws/asg-ebs module was used as a basis, adding minimal changes to work without an EBS volume.

--- main.tf
+++ main.tf
@@ -1,5 +1,128 @@
-
-// TODO: define locals at the top of main.tf
-
+/*
+ * Generate user_data from template file
+ */
 locals {
+  user_data = templatefile("${path.module}/user-data.sh", {
+    ecs_cluster_name     = var.ecs_cluster_name
+    additional_user_data = var.additional_user_data
+  })
+
+  user_data_ebs = templatefile("${path.module}/user-data-ebs.sh", {
+    ecs_cluster_name     = var.ecs_cluster_name
+    additional_user_data = var.additional_user_data
+    aws_region           = var.aws_region
+    aws_access_key       = var.aws_access_key
+    aws_secret_key       = var.aws_secret_key
+    ebs_device           = var.ebs_device
+    ebs_mount_point      = var.ebs_mount_point
+    ebs_vol_id           = var.ebs_vol_id
+    ebs_mkfs_label       = var.ebs_mkfs_label
+    ebs_mkfs_labelflag   = var.ebs_mkfs_labelflag
+    ebs_mkfs_extraopts   = var.ebs_mkfs_extraopts
+    ebs_fs_type          = var.ebs_fs_type
+    ebs_mountopts        = var.ebs_mountopts
+  })
+
+  credits = var.cpu_credits == "" ? [] : [var.cpu_credits]
+}
+
+/*
+ * Create Launch Template
+ */
+resource "aws_launch_template" "asg_lt" {
+  ebs_optimized          = false
+  name                   = "lt-${var.app_name}-${var.app_env}"
+  image_id               = var.ami_id
+  instance_type          = var.aws_instance["instance_type"]
+  key_name               = var.key_name
+  update_default_version = true
+  user_data              = base64encode(var.ebs_device == "" ? local.user_data : local.user_data_ebs)
+
+  block_device_mappings {
+    device_name = var.root_device_name
+    ebs {
+      delete_on_termination = true
+      volume_size           = var.aws_instance["volume_size"]
+    }
+  }
+
+  dynamic "credit_specification" {
+    iterator = ii
+    for_each = local.credits
+    content {
+      cpu_credits = ii.value
+    }
+  }
+
+  network_interfaces {
+    associate_public_ip_address = var.associate_public_ip_address
+    security_groups             = concat([var.default_sg_id], var.additional_security_groups)
+    ipv6_address_count          = var.enable_ipv6 ? 1 : 0
+  }
+
+  iam_instance_profile {
+    name = var.ecs_instance_profile_id
+  }
+
+  monitoring {
+    enabled = var.enable_ec2_detailed_monitoring
+  }
+
+  dynamic "tag_specifications" {
+    for_each = ["network-interface", "volume"]
+    iterator = resource
+
+    content {
+      resource_type = resource.value
+
+      tags = var.tags
+    }
+  }
+}
+
+/*
+ * Create Auto Scaling Group
+ */
+resource "aws_autoscaling_group" "asg" {
+  name                      = "asg-${var.app_name}-${var.app_env}"
+  vpc_zone_identifier       = var.private_subnet_ids
+  min_size                  = var.aws_instance["instance_count"]
+  max_size                  = var.aws_instance["instance_count"]
+  desired_capacity          = var.aws_instance["instance_count"]
+  health_check_type         = "EC2"
+  health_check_grace_period = "120"
+  default_cooldown          = "30"
+
+  launch_template {
+    id      = aws_launch_template.asg_lt.id
+    version = "$Latest"
+  }
+
+  tag {
+    key                 = "Name"
+    value               = "${var.app_name}-${var.app_env}"
+    propagate_at_launch = true
+  }
+
+  tag {
+    key                 = "app_name"
+    value               = var.app_name
+    propagate_at_launch = true
+  }
+
+  tag {
+    key                 = "app_env"
+    value               = var.app_env
+    propagate_at_launch = true
+  }
+
+  dynamic "tag" {
+    for_each = var.tags
+
+    content {
+      key                 = tag.key
+      value               = tag.value
+      propagate_at_launch = true
+    }
+  }
 }
--- test/main.tf
+++ test/main.tf
@@ -1,26 +1,53 @@
 
-/*
- * TODO: complete these basic instantiations of the module, with the base purpose of
- * validating the syntax of module code automatically when pushed to version control.
- * One instance should use the minimum allowable set of inputs. The other should have
- * the full complement of inputs. You may also wish to include module outputs to
- * enforce the presence of module outputs.
- */
-
 module "minimal" {
   source = "../"
 
-  variable_name = "foo"
+  ami_id                  = ""
+  private_subnet_ids      = []
+  default_sg_id           = ""
+  ecs_instance_profile_id = ""
+  ecs_cluster_name        = ""
 }
 
 module "full" {
   source = "../"
 
-  variable_name = "foo"
+  app_name                       = ""
+  app_env                        = ""
+  aws_access_key                 = ""
+  aws_secret_key                 = ""
+  ami_id                         = ""
+  associate_public_ip_address    = false
+  aws_instance                   = {}
+  cpu_credits                    = ""
+  root_device_name               = ""
+  aws_region                     = ""
+  private_subnet_ids             = []
+  default_sg_id                  = ""
+  ecs_instance_profile_id        = ""
+  ecs_cluster_name               = ""
+  key_name                       = ""
+  additional_security_groups     = []
+  additional_user_data           = ""
+  tags                           = {}
+  ebs_device                     = ""
+  ebs_mount_point                = ""
+  ebs_vol_id                     = ""
+  ebs_mkfs_label                 = ""
+  ebs_mkfs_labelflag             = ""
+  ebs_mkfs_extraopts             = ""
+  ebs_mountopts                  = ""
+  ebs_fs_type                    = ""
+  enable_ec2_detailed_monitoring = false
+  enable_ipv6                    = false
 }
 
-output "an_output" {
-  value = module.minimal.output_name
+output "launch_template_id" {
+  value = module.minimal.launch_template_id
+}
+
+output "auto_scaling_group_id" {
+  value = module.minimal.auto_scaling_group_id
 }
 
 provider "aws" {
@@ -28,12 +55,12 @@ provider "aws" {
 }
 
 terraform {
-  required_version = ">= 1.0"
+  required_version = ">= 0.12"
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.0"
+      version = ">= 4.0.0, < 6.0.0"
     }
   }
 }
